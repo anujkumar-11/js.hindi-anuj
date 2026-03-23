4c41c80e90427c1fd926004f3f2da5b08e0dc534
anujkumar-11 <24bec021@nith.ac.in>
2026-03-23 20:47:28 +0000

    conveersionOfDatatypes

---
+++ 01-basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "33abs"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+/*
+  "33"=>33
+  "33abs"=>NaN
+  true=>1 false=>0
+
+*/
+  //boolean
+let isloggedIn=1
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1=>true 0=>false
+// ""=>false
+// "hitesh"=>true
+
+   //string conversion
+   let somenumber=33;
+   let stringNUMber=String(somenumber)
+   console.log(stringNUMber)
+   console.log(typeof stringNUMber);
