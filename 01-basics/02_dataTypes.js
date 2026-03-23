5d5ae2633c5c8658f989d27af58029acb353a4db
anujkumar-11 <24bec021@nith.ac.in>
2026-03-23 20:15:51 +0000

    datatypes

---
+++ 01-basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+// alert( 3+3) // we are using nodejs, not browser
+
+let name = "hitesh"
+let age = 18 
+let isLoggedIn = false
+
+ // permitive date type 
+//number => range 2 to power 53
+//bigint => for lager number 
+//string =>use with single/double quote perfer double quote
+//boolean =>true/false
+//null =>standlone value, ye undefined nhi hai ye khali hai 
+//undefined =>means abhi tk value define nhi hui hai 
+//symbol =>use for uniqueness 
+
+//object 
+
+console.log(typeof isLoggedIn);
+console.log(typeof null);
+console.log(typeof undefined);
