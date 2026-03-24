8da3ec88bb242555fe10f44649f8579d5936f97b
anujkumar-11 <24bec021@nith.ac.in>
2026-03-24 18:06:04 +0000

    comparision

--- 01-basics/03_conversionOperation.js
+++ 01-basics/03_conversionOperation.js
@@ -31,3 +31,48 @@ console.log(booleanIsLoggedIn);
    let stringNUMber=String(somenumber)
    console.log(stringNUMber)
    console.log(typeof stringNUMber);
+
+
+
+  //  ********* OPERATIONS *******
+        
+  let value =3
+  let negvalue = -value
+  console.log(negvalue)
+
+  console.log(2+2);
+   console.log(2-2);
+    console.log(2*2);
+     console.log(2**3);
+     console.log(2/3)
+     console.log(2%3)
+
+     let str1="hello"
+     let str2=" Hitesh"
+     let str3 = str1 + str2;
+     console.log(str3);
+
+     console.log("1" + 2)
+     console.log(1 + "2")
+     console.log("1" + 2 + 2)
+     console.log(1 + 2 +"3")
+
+     console.log( 3+ 4 *5 % 3)// ues parnthesis instead of this 
+
+     console.log(true) //output =>> true;
+     console.log(+true)//output =>> 1;
+     console.log(+"") //output =>> 0;
+
+     // assignment operatot
+
+     let num1, num2, num3
+     num1=num2=num3=2+2;
+     let gamecounter =100;
+     gamecounter++;
+     console.log(gamecounter);
+      
+
+
+// link to study conversion
+//https:tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
---
+++ 01-basics/04_conparision.js
@@ -0,0 +1,23 @@
+console.log(2>1) // output in boolean
+console.log(2>=1)
+console.log(2<1)
+
+
+// problem when different data type conversion
+//while comparing ensure that both are same data type
+
+console.log("2">1)
+
+// is tarah ke conparision ko avoid karte hai 
+
+console.log(null>0); //false
+console.log(null==0);//false
+console.log(num>=0);//true
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+
+// === strict check
+ console.log("2"==2);
