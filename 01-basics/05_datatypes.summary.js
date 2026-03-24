4061dd0d0d128e41b8eebef6e55e5c50875b82c8
anujkumar-11 <24bec021@nith.ac.in>
2026-03-24 18:58:46 +0000

    datatypes_summary

---
+++ 01-basics/05_datatypes.summary.js
@@ -0,0 +1,66 @@
+//javascrit is dynamic data type 
+// dyanamic Type runtime par decide hota hai
+            //Variable ka type change ho sakta hai
+
+//2 type of data type 
+
+ //1 => primitive (call by value)
+     // string , number , boolean, null , undefined,
+     // symbol , bigint
+
+//2 => non primitive (refrence)
+
+     // array , objects , functions
+
+
+     1 
+
+const score = 100;
+const scoreValue = 100.3;
+
+const islogin = false;
+const outsidetemp =null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123') // both are not same 
+
+console.log(id ===anotherId);
+
+const bigNumber = 3456789754n
+
+ 2 
+
+ const heroes = ["shaktiman", "naagraj" , "doga"] //array
+ let myObj = {
+    name: "hitesh",
+    age: 22,     // object
+ }
+
+ //function
+ const myFunction = function() {
+     console.log("Hello world");
+
+ }
+
+
+
+ //hw type of all data type with (typeof)
+     // Undefinde => undefined
+     // Null => object
+     // Boolean=> boolean
+     //Number=> number
+     //String => string 
+     // object => object
+     // funtion =>function object
+     //array=>object;
+// dataType of all non_primitive data type is "Object"
+
+
+
+ 
+
+
+
+
+
