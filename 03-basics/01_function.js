5de53d8433bc828c934bf3c88cd8f38478293907
anujkumar-11 <24bec021@nith.ac.in>
2026-06-25 08:57:38 +0000

    function

---
+++ 03-basics/01_function.js
@@ -0,0 +1,45 @@
+function sayMyName (){
+    console.log("h");
+    console.log("i");
+    console.log("t");
+    console.log("e");
+    console.log("s");
+    console.log("h");
+}
+
+// sayMyName ==> this is function refrence 
+// sayMyName() ==> this is fuction execution
+
+sayMyName();
+
+
+// function addTwoNumbers(number1,number2){ // number1 and number2 => called parameters 
+
+//     console.log(number1+ number2);
+// }
+// addTwoNumbers(3,"a"); // 3 , a => called arguments 
+
+function addTwoNumbers(number1, number2) {
+
+  let result = number1 + number2
+  return result
+ 
+}
+const result = addTwoNumbers(3,5)
+console.log("result : ", result )
+
+
+function loginUserMessage(username="sam"){  // username ="sam" default value 
+    if(username === undefined){
+        console.log("Please enter a username");
+        return 
+    }
+    return `${username} just logged in`
+
+}
+
+ console.log(loginUserMessage("hitesh"));  // jb isme koi value nhi pass karenge to undefined aaiga
+
+
+
+
