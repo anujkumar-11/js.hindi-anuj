1ca779d3f9f3c3bf7cfb906e6c43516b28618bb0
anujkumar-11 <24bec021@nith.ac.in>
2026-07-22 01:18:35 +0530

    Implement control flow examples in one.js
    Added control flow examples using if statements and logical operators.

---
+++ 05_control_flow/one.js
@@ -0,0 +1,33 @@
+
+// ********* if ************
+
+// if(condition=> true){
+// }
+
+
+// comparision operator 
+
+   // < , > , >=,<=,==, != , ===(it check type also),!==(check negative sign with type)
+
+
+// const balance = 1000;
+
+// if(balance<500){
+//     console.log("less than ")
+// }
+// else if(balance<750){
+//     console.log("less than 750")
+// }
+// else if(balance<900){
+//     console.log("less than 900")
+// }else{
+//     console.log("less than 1200")
+// }
+
+
+const userLoggedIn = true;
+const debitCard = true;
+
+if(userLoggedIn && debitCard){
+    console.log("Allow to buy course")
+}
